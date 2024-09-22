33d8857d7cdef92af387077a69819f57d522b129
Alla-java <goneman@yandex.ru>
2024-09-22 12:35:26 +0300

    Внесены исправления в задания 3 и 4

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        else if (clientOS == 1) {System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
 
         /* Задание 2. */
@@ -18,8 +18,7 @@ public class Main {
         } else {
             operationSystem = "Android";
         }
-        if (clientDeviceYear < 2015)
-        {
+        if (clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для" + " " + operationSystem + " " + "по ссылке");
         } else {
             System.out.println("Установите версию приложения для" + " " + operationSystem + " " + "по ссылке");
@@ -31,9 +30,7 @@ public class Main {
 
         if ((year % 4 == 0 && year % 100 != 0 && year > 1584) || (year % 400 == 0 && year > 1584)) {
             System.out.println(year + " год является високосным");
-        }
-
-        else {
+        } else {
             System.out.println(year + " " + "год не является високосным");
         }
 
@@ -42,17 +39,11 @@ public class Main {
 
         if (deliveryDistance < 20) {
             System.out.println("Потребуется дней: 1");
-        }
-
-        else if (deliveryDistance < 60 && deliveryDistance >= 20) {
+        } else if (deliveryDistance < 60 && deliveryDistance >= 20) {
             System.out.println("Потребуется дней: 2");
-        }
-
-        else if (deliveryDistance < 100 && deliveryDistance >= 60) {
+        } else if (deliveryDistance < 100 && deliveryDistance >= 60) {
             System.out.println("Потребуется дней: 3");
-        }
-
-        else if (deliveryDistance >= 100) {
+        } else if (deliveryDistance >= 100) {
             System.out.println("Доставки нет");
         }
 
@@ -72,7 +63,7 @@ public class Main {
             case 9, 10, 11:
                 System.out.println("Осень");
                 break;
-            default :
+            default:
                 System.out.println("Такого месяца не существует");
         }
     }
